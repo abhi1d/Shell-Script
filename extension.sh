faa3226acd838a9f176f66db3a86bfc0b1b27dd7
Abhishek sharma <abhiislikeme@gmail.com>
2017-05-28 01:10:07 +0530

    Create extension.sh

---
+++ extension.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# extension .c files will go into the C_CODES folder
+mv /home/abhi/*.c /home/abhi/C_CODES/  
+
+# extension .cpp files will go into the CPP_CODES folder
+mv /home/abhi/*.cpp /home/abhi/CPP_CODES/
+
+# extension .java files will go into the JAVA_CODES folder    
+mv /home/abhi/*.java /home/abhi/JAVA_CODES/
+
+ 
+ 
